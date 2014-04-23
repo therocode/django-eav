6012dfbd14eeea0a38ac2fc8104f35cedc58aea8
colin <colinkingswood@gmail.com>
2014-04-23 16:12:49 +0200

    changed imports

--- eav/__init__.py
+++ eav/__init__.py
@@ -27,9 +27,9 @@ def get_version():
 __version__ = get_version()
 
 def register(model_cls, config_cls=None):
-    from eav.registry import Registry
+    from .registry import Registry
     Registry.register(model_cls, config_cls)
 
 def unregister(model_cls):
-    from eav.registry import Registry
+    from .registry import Registry
     Registry.unregister(model_cls)
